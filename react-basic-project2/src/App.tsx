8d0153fd9bc89d6a0a23490ae06609757035178b
srwoo <rhak12@naver.com>
2023-03-05 18:17:48 +0900

    init

--- react-basic-project2/src/App.tsx
+++ react-basic-project2/src/App.tsx
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled/macro';
+import Skeleton from './components/Skeleton';
+
+const Base = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 12px;
+  row-gap: 24px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+  border-radius: 4px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Info = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0%;
+`;
+
+const Title = styled.h4`
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+`;
+
+const Description = styled.p`
+  margin: 8px 0 0 0;
+  padding: 0;
+  font-size: 16px;
+`;
+
+const Placeholder: React.FC = () => ( // <Item /> 에 대응하는 Placeholder 제작
+  <Container>
+    <ImageWrapper>
+      <Skeleton width={320} height={220} />
+    </ImageWrapper>
+    <Info>
+      <Skeleton width={150} height={29} rounded />
+      <div style={{ height: '8px' }} />
+      <Skeleton width={200} height={19} rounded />
+    </Info>
+  </Container>
+)
+
+const Item: React.FC = () => ( // 실제 보여줄 컨텐츠
+  <Container>
+    <ImageWrapper>
+      <Image src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg" />
+    </ImageWrapper>
+      <Info>
+        <Title>Cat taking a nap</Title>
+        <Description>zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</Description>
+      </Info>
+  </Container>
+)
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => { // 임의로 로딩 상태 표현
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
   return (
-    <div className="App">
-      good
-    </div>
+    <Base>
+      {
+        loading ?  Array.from({ length: 25 }).map((_, idx) => (
+          <Placeholder key={idx} />
+        )) : Array.from({ length: 25 }).map((_, idx) => (
+          <Item key={idx} />
+        ))
+      }
+    </Base>
   );
 }
 
